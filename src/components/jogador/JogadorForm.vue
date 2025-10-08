ed097075b3ac7a9755a0dfa2d79004656f298294
Isaias Santos <95.isaiassantos@gmail.com>
2025-10-07 22:48:28 -0300

    defineção do form para jogador

---
+++ src/components/jogador/JogadorForm.vue
@@ -0,0 +1,27 @@
+<template>
+
+</template>
+
+<script setup lang="ts">
+import type { Jogador } from '@/types/typesJogador';
+import { reactive } from 'vue';
+
+    
+
+const jogadorForm = reactive<Jogador>({
+    id: 0,
+    nome: '',
+    numeroCamisa: '',
+    posicao: 'Goleiro',
+    dataNascimento: '',
+    naturalidade: '',
+    nacionalidade: '',
+    altura: 0,
+    pePreferido: 'Direito',
+    telefone: '',
+    email: '',
+    documento: '',
+    situacao: 'Ativo'
+});
+
+</script>
